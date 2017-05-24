cccec7339a3c1f9a070a1ad3428dfb7227e3ddc9
Hackzzila <admin@hackzzila.com>
2017-05-23 19:01:58 -0500

    update registry url

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
 // Gets the registry to use.
 export function registry() { // eslint-disable-line
-  return localStorage.registry || 'https://registry.hackzzila.com';
+  return localStorage.registry || 'https://registry.diamondpkg.org';
 }
